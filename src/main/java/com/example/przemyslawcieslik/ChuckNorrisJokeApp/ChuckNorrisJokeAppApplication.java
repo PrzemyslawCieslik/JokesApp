a85ceaadc42b0216867b5a9d134337e1d260a2b5
Przemyslaw <cprzemek91@hotmail.com>
2019-01-28 21:53:48 +0000

    Base File

---
+++ src/main/java/com/example/przemyslawcieslik/ChuckNorrisJokeApp/ChuckNorrisJokeAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.przemyslawcieslik.ChuckNorrisJokeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorrisJokeAppApplication.class, args);
+	}
+
+}
+
