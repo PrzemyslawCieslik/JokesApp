9dcdb2f2e9e0eff976997094370ceee62909310e
Przemyslaw <cprzemek91@hotmail.com>
2019-02-08 19:36:25 +0000

    beans

--- src/main/java/com/example/przemyslawcieslik/ChuckNorrisJokeApp/ChuckNorrisJokeAppApplication.java
+++ src/main/java/com/example/przemyslawcieslik/ChuckNorrisJokeApp/ChuckNorrisJokeAppApplication.java
@@ -2,8 +2,10 @@ package com.example.przemyslawcieslik.ChuckNorrisJokeApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:templates/chuckConfig.xml")
 public class ChuckNorrisJokeAppApplication {
 
 	public static void main(String[] args) {
